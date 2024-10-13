53f58c79f1871e307da0826754e85fb561c1d02d
OrzMiku <miku@ecy.pink>
2024-10-13 13:30:54 +0800

    Qingqiu document init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,49 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+const locales = {
+	'zh-cn': {
+		label: '简体中文',
+		lang: 'zh-CN',
+	},
+	'en': {
+		label: 'English',
+		lang: 'en',
+	},
+};
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: {
+				'zh-CN': '青丘',
+				'en': 'Qingqiu',
+			},
+			defaultLocale: 'zh-cn',
+			locales,
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/OrzMiku/docs',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: '青丘',
+					translations: {
+						'en': 'Qingqiu',
+					},
+					slug: 'qingqiu',
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					label: 'LunarFox 整合包',
+					translations: {
+						'en': 'LunarFox Modpack',
+					},
+					autogenerate: {
+						directory: 'lunarfox',
+					},
+				}
 			],
+			favicon: './src/assets/avatarHead.png',
 		}),
 	],
 });
